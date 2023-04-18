08664712191fbde57aa9709ce4d82dafe595ab3e
Conor Murphy <91476339+ConorPadraigMurphy@users.noreply.github.com>
2023-04-18 21:45:21 +0100

    Added info into about page

--- src/componenets/About.js
+++ src/componenets/About.js
@@ -1,10 +1,13 @@
 import React from "react";
-
+import Card from 'react-bootstrap/Card';
 export class About extends React.Component {
     render() {
         return (
-            <div>
-                <h2>About Page</h2>
+            <div className="App" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '20px' }}>
+                <Card style={{ width: '90%' }}>
+                    <Card.Header><h3>About</h3></Card.Header>
+                    <Card.Body>This website was developed by Rohan and Conor for our Professional practice in IT assignment in our third year of computing in software development in ATU</Card.Body>
+                </Card>
             </div>
         );
     }
